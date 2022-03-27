ba1f6a838dedc3599f601c6fb09c664bef502926
JAIME GARCIA <jgo962@inlumine.ual.es>
2022-03-27 16:05:52 +0200

    Subida vista usuario no registrado - buena

---
+++ mds2/src/main/java/vistas/VistaCabeceradeinicio.java
@@ -0,0 +1,26 @@
+package vistas;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.template.Id;
+
+/**
+ * A Designer generated component for the vista-cabeceradeinicio template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("vista-cabeceradeinicio")
+@JsModule("./vista-cabeceradeinicio.ts")
+public class VistaCabeceradeinicio extends LitTemplate {
+
+    /**
+     * Creates a new VistaCabeceradeinicio.
+     */
+    public VistaCabeceradeinicio() {
+        // You can initialise any data required for the connected UI components here.
+    }
+
+}
