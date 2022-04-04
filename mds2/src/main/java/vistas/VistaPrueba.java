115e4553c1320d59c3e6ca6915c21797904f999c
Adrián Rodríguez Manzano <arm115@inlumine.ual.es>
2022-04-04 13:33:12 +0200

    Subida de prueba a la rama dev

---
+++ mds2/src/main/java/vistas/VistaPrueba.java
@@ -0,0 +1,24 @@
+package vistas;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+
+/**
+ * A Designer generated component for the vista-prueba template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("vista-prueba")
+@JsModule("./vista-prueba.ts")
+public class VistaPrueba extends LitTemplate {
+
+    /**
+     * Creates a new VistaPrueba.
+     */
+    public VistaPrueba() {
+        // You can initialise any data required for the connected UI components here.
+    }
+
+}
