b1891ab32b1a2901037ca3546975b046ca592908
Adrián Rodríguez Manzano <arm115@inlumine.ual.es>
2022-04-18 14:22:24 +0200

    Subida archivos java que faltaban

---
+++ mds2/src/main/java/vistas/VistaCabeceraadministrador.java
@@ -0,0 +1,24 @@
+package vistas;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+
+/**
+ * A Designer generated component for the vista-cabeceraadministrador template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("vista-cabeceraadministrador")
+@JsModule("./vista-cabeceraadministrador.ts")
+public class VistaCabeceraadministrador extends LitTemplate {
+
+    /**
+     * Creates a new VistaCabeceraadministrador.
+     */
+    public VistaCabeceraadministrador() {
+        // You can initialise any data required for the connected UI components here.
+    }
+
+}
